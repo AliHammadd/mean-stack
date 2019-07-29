4ecc21e4a042ab81129ac4cc9d2ed977bcb231f2
Ali Hammad <alihammad56@gmail.com>
2019-07-29 17:37:34 +0500

    Re-Organizing Backend Routes

--- backend/app.js
+++ backend/app.js
@@ -10,8 +10,6 @@ mongoose.connect('mongodb://localhost:27017/meanstack', {useNewUrlParser: true})
     console.log('Connection failed');
 });
 
-const Post = require('./models/post');
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -23,57 +21,8 @@ app.use((req, res, next)=> {
     next();
 });
 
-app.post('/api/posts',(req, res, next) => {
-    const post = new Post({
-        title: req.body.title,
-        content: req.body.content
-    });
+const postRoutes = require('./routes/posts');
 
-    post.save().then((createdPost) =>{
-        res.status(201).json({
-            postId: createdPost._id,
-            message: 'Successfully posted',
-        });
-    });
-});
+app.use('/api/posts', postRoutes);
 
-app.put('/api/posts/:id', (req, res, next) => {
-    const post = new Post({
-        _id: req.body.id,
-        title: req.body.title,
-        content: req.body.content
-    });
-    Post.updateOne({_id: req.params.id}, post).then((result)=> {
-        console.log(result);
-        res.status(200).json({message: 'Update Successfully!'});
-    })
-});
-
-
-app.get('/api/posts/:id', (req, res, next) =>{
-    Post.findById(req.params.id).then(post=> {
-        if(post){
-            res.status(200).json(post);
-        }else{
-            res.status(404).json({ message: 'Post not found' });
-        }
-    });
-});
-
-app.get('/api/posts', (req, res, next) => {
-
-    Post.find().then(documents => {
-        res.status(200).json({
-            'message': 'Post send successfully',
-            'posts': documents
-        });
-    });
- });
-
- app.delete('/api/posts/:id', (req, res, next) => {
-    Post.deleteOne({ _id: req.params.id }).then((result) => {
-        res.status(200).json({ message: 'Post deleted!' });
-    });
- });
-
- module.exports = app;
+module.exports = app;
